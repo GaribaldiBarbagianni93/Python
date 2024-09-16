6088ea9e3d58495b9abcbf4a9345a34cf42fae37
Gabriele <gabriele.monasterolo@itiscuneo.eu>
2024-09-16 13:19:56 +0200

    main

---
+++ PrimoFile.py
@@ -0,0 +1,10 @@
+#tabelllina del num
+#range(inizio, fine, step)
+#input resituisce sempre una string devo sempre trasformarla detto casting
+
+
+num= int(input)
+input(num)
+for i in range(num, (num*10)+1, num):
+    print(i)
+
