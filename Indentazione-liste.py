7d9c5c5096de702b07074d05ea7679a05418371d
Gabriele <gabriele.monasterolo@itiscuneo.eu>
2024-09-23 12:23:18 +0200

    Nuovo File Liste

---
+++ Indentazione-liste.py
@@ -0,0 +1,10 @@
+lista = [ [{1, 2, 3, 7}, {4, 5, 6}], [{7, 8, 9}, {10, 11, 12, 21}, {3, 4, 11}], [{13, 14, 15}, {16, 17, 18}] ]
+
+#provoni
+print(f"Lunghezza lista: {len(lista)}")
+print(f"Lunghezza lista[0]: {len(lista[0])}")
+print(f"Lunghezza lista[0][0]: {len(lista[0][0])}")
+print(f"Lunghezza lista[0][1]: {len(lista[0][1])}")
+
+
+
